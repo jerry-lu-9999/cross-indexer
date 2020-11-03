06d9ae7494583c8cd172ee5fea7b2e7635911eae
Thu Hoang Phan Ha <thoangph@cycle1.csug.rochester.edu>
2020-11-03 00:15:15 -0500

    out file

--- xref.py
+++ xref.py
@@ -1,8 +1,13 @@
 import subprocess
+from subprocess import call
 
 subprocess.call("date", shell=True)
 
-subprocess.run(["objdump", "-d", "main"], check=True)
+f = open('objdump.txt', 'w')
+call(["objdump", "-d", "example"], stdout=f)
+f.close()
 
-subprocess.run(["llvm-dwarfdump", "-o", "output.txt", "--debug-line", "main"], check=True)
-
+f = open('output.txt', 'w')
+# subprocess.run(["llvm-dwarfdump", "-o", "output.txt", "--debug-line", "main"], check=True)
+call(["llvm-dwarfdump", "-o", "output.txt", "--debug-line", "example"], stdout=f)
+f.close()
