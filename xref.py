1e93af9bef85347ac5d0371a4b693a60bbe36a03
yanyujianshen <ljh274163788@gmail.com>
2020-11-03 04:21:14 -0500

    working on output

--- xref.py
+++ xref.py
@@ -1,5 +1,8 @@
 import subprocess
+import re
 from subprocess import call
+
+
 
 subprocess.call("date", shell=True)
 
@@ -11,3 +14,25 @@ f = open('output.txt', 'w')
 # subprocess.run(["llvm-dwarfdump", "-o", "output.txt", "--debug-line", "main"], check=True)
 call(["llvm-dwarfdump", "-o", "output.txt", "--debug-line", "example"], stdout=f)
 f.close()
+
+# 715
+line_number = 0
+pattern = re.compile("(0x0{12})(\d{4})")
+with open('output.txt', 'r') as llvm:
+    for line in llvm:
+        line_number += 1
+        if "example.rs" in line:
+            break
+    for i in range(line_number, line_number + 6):
+        llvm.readline()
+    for line in llvm:
+        if line.strip() == '':
+            break
+        m = pattern.match(line)
+        # if matched, we take out the actually address part
+        if m:
+            memory_addr = m.group(2)
+            print(memory_addr)
+            line_num = line.split("\s+")[1]
+            print(line_num)
+
